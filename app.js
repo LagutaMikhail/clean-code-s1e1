d102d1371f637c2639e9a3294e258ce869da4743
LagutaMikhail <lagutamihail@gmail.com>
2022-11-01 00:26:54 +0300

    refactor: add BEM notation according to par. 2.1 of advanced level rules

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
+    var containsClass=listItem.classList.contains("list__item_edit");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -107,7 +107,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("edit-mode");
+    listItem.classList.toggle("list__item_edit");
 };
 
 
